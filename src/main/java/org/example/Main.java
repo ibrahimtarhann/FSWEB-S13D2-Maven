3987a5908bc22135294142d031de4758f2a0aa44
i-7-ibrahim <m4tr1x700@hotmail.com>
2025-01-09 21:17:46 +0300

    Finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(isPalindrome(1221));
         System.out.println(isPerfectNumber(28));
+
+
     }
 
     public static boolean isPalindrome(int number) {
